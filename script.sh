12093bf637305abfe6d0123c60adfb99cf90e1e0
terrensmint <rem.ruslan2007@gmail.com>
2025-11-09 19:49:12 +0300

    Реализация сортировки

--- script.sh
+++ script.sh
@@ -5,9 +5,18 @@
 
 # Функция для сортировки чисел
 sort_numbers(){
-	arr=("$@")	# Массив со всеми аргументами
-
-	# Сортировка
+	# Сортировка пузырьком
+    local arr=("$@") 
+    local n=${#arr[@]}
+    for ((i = 0; i < n - 1; i++)); do
+        for ((j = 0; j < n - i - 1; j++)); do
+            if [ ${arr[j]} -gt ${arr[$((j + 1))]} ]; then
+                local temp=${arr[j]}
+                arr[j]=${arr[$((j + 1))]}
+                arr[$((j + 1))]=$temp
+            fi
+        done
+    done
 
 	echo "${arr[@]}" # Возвращаем отсортированный массив
 }
